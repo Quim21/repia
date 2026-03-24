d0a71c47f6239265f014b69fb559961d3d9b51c9
IzanLosada <alu.izan.losada@mataro.epiaedu.cat>
2026-03-24 17:41:09 +0100

    Add files via upload

---
+++ js/config.js
@@ -0,0 +1,11 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyDrCUIQ41uibOMscCMzv52zVFoSO2DBxZ8",
+  authDomain: "repia-e9dfd.firebaseapp.com",
+  projectId: "repia-e9dfd",
+  storageBucket: "repia-e9dfd.firebasestorage.app",
+  messagingSenderId: "1019919912911",
+  appId: "1:1019919912911:web:8ee411e3bb1bccd31f4cba",
+  measurementId: "G-ZD5RQZL1TE"
+});
+
+const auth = firebase.auth();
